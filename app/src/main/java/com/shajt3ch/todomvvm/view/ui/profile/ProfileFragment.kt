73c60c5a2730620c159fe54bd67b02de7b07067f
SHaZ <shakilahmedshaj@gmail.com>
2020-04-20 19:25:55 +0600

    image load solved

--- app/src/main/java/com/shajt3ch/todomvvm/view/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/shajt3ch/todomvvm/view/ui/profile/ProfileFragment.kt
@@ -2,16 +2,23 @@ package com.shajt3ch.todomvvm.view.ui.profile
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 
 import com.shajt3ch.todomvvm.R
+import com.shajt3ch.todomvvm.viewmodel.profile.ProfileViewModel
+import kotlinx.android.synthetic.main.profile_fragment.*
 
 class ProfileFragment : Fragment() {
 
     companion object {
+        const val TAG = "ProfileFragment"
         fun newInstance() = ProfileFragment()
     }
 
