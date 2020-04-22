3b4701b744a15596afccf76e83c45ad9c9d6d193
SHaZ <shakilahmedshaj@gmail.com>
2020-04-23 00:54:53 +0600

    Refactor Login ViewModel and Activity

--- app/src/main/java/com/shajt3ch/todomvvm/viewmodel/auth/LoginViewModel.kt
+++ app/src/main/java/com/shajt3ch/todomvvm/viewmodel/auth/LoginViewModel.kt
@@ -1,8 +1,11 @@
 package com.shajt3ch.todomvvm.viewmodel.auth
 
+import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.shajt3ch.todomvvm.BuildConfig
@@ -19,43 +22,46 @@ import kotlin.Exception
  * Created by Shakil Ahmed Shaj on 14,April,2020.
  * shakilahmedshaj@gmail.com
  */
-class LoginViewModel : ViewModel() {
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     companion object {
         const val TAG = "LoginViewModel"
     }
 
     private val networkService = Networking.create(BuildConfig.BASE_URL)
+    private var loginRepository: LoginRepository
+    private var sharedPreferences: SharedPreferences =
+        application.getSharedPreferences("com.shajt3ch.todomvvm.pref", Context.MODE_PRIVATE)
+    private var appPreferences: AppPreferences
+    val loginResponse: MutableLiveData<LoginResponse> = MutableLiveData()
+    val isSuccess: MutableLiveData<Boolean> = MutableLiveData()
+    val isError: MutableLiveData<String> = MutableLiveData()
 
-    private lateinit var loginRepository: LoginRepository
-
-    private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var appPreferences: AppPreferences
-
-    fun init(context: Context) {
-        sharedPreferences =
-            context.getSharedPreferences("com.shajt3ch.todomvvm.pref", Context.MODE_PRIVATE)
+    init {
         appPreferences = AppPreferences(sharedPreferences)
         loginRepository = LoginRepository(networkService, appPreferences)
     }
 
-
-    fun login(loginRequest: LoginRequest) = liveData(IO) {
+    fun login(loginRequest: LoginRequest) = liveData {
 
         try {
-
+            val data = loginRepository.login(loginRequest)
+            if (data.code() == 200) {
+                loginResponse.value = data.body()
+                emit(loginResponse.value)
+                isSuccess.value = true
+            } else {
+                isSuccess.value = false
+            }
         } catch (httpException: HttpException) {
             Log.e(TAG, httpException.toString())
+            isError.value = httpException.toString()
 
 
         } catch (exception: Exception) {
             Log.e(TAG, exception.toString())
-
+            isError.value = exception.toString()
         }
-
-
-        val data = loginRepository.login(loginRequest)
-        emit(data)
 
     }
 
